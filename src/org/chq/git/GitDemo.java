e98034226d9092e259682206f0068001bc22d0ca
rbrother <honkey666@163.com>
2019-08-02 14:30:59 +0800

    GitDemo

---
+++ src/org/chq/git/GitDemo.java
@@ -0,0 +1,13 @@
+package org.chq.git;
+
+/**
+* @author rbrother
+* @version 创建时间：2019年8月2日 下午2:21:53
+* 类说明
+*/
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("***************** hello git *****************");
+	}
+}
